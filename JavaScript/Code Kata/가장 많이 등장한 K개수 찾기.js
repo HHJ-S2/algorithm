149cea3b31dc90487a305fcf248f345eddda4882
HHJ-S2 <berdle0122@gmail.com>
2020-12-22 11:08:58 +0900

    Add: 가장 많이 등장한 K개수 찾기

---
+++ JavaScript/Code Kata/가장 많이 등장한 K개수 찾기.js
@@ -0,0 +1,17 @@
+function topK(nums, k) {
+  let obj = {};
+  
+  for (let i=0; i < nums.length; i++) {
+    (nums[i] in obj) ?
+    obj[nums[i]] += 1
+    : obj[nums[i]] = 1
+  }
+  
+  let test = Object.keys(obj).sort(function (a, b) {
+    return obj[b] - obj[a];
+  });
+  
+  return test.slice(0, k).map( x => parseInt(x));
+}
+
+topK([1,1,1,2,2,3],2);
